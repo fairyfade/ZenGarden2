77748710902c12228132759ead8e1014458f5434
fairyfade <77501073+fairyfade@users.noreply.github.com>
2021-04-29 16:27:29 -0400

    LanternLevelScene

---
+++ Assets/Scripts/ChangeSky.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeSky : MonoBehaviour
+{
+    //resource used http://www.bigfatblueberry.com/unity3d-how-to-change-skybox-with-script/
+
+    //assign skybox materials
+    public Material sunriseSkybox;
+    public Material midDaySkybox;
+    public Material sunsetSkybox;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    //change skybox to sunrise
+    void sunrise()
+    {
+        RenderSettings.skybox = sunriseSkybox;
+    }
+
+    //change skybox to midDay
+    void midDay()
+    {
+        RenderSettings.skybox = midDaySkybox;
+    }
+
+    //change skybox to sunset
+    void sunset()
+    {
+        RenderSettings.skybox = sunsetSkybox;
+    }
+}
