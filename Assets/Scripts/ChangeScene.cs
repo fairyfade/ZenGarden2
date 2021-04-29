7b09e85da8bf92b971d971dc24c25d96285b8256
fairyfade <77501073+fairyfade@users.noreply.github.com>
2021-04-29 17:25:07 -0400

    Lanterns should work

---
+++ Assets/Scripts/ChangeScene.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ChangeScene : MonoBehaviour
+{
+    private bool HAS_FIRED;
+    // Start is called before the first frame update
+    void Start()
+    {
+        HAS_FIRED = false;
+    }
+
+    public void nextLevel()
+    {
+        if (HAS_FIRED == false)
+        {
+            //progress to next level 
+            SceneManager.LoadScene("Level2", LoadSceneMode.Additive);
+            HAS_FIRED = true;
+        }
+
+    }
+}
+
